b0b7c1fcab2e688eb30ba7708b9c5bc093b6029a
Sylvia Lei <lixlei@microsoft.com>
2023-03-29 15:13:29 +0800

    feat: define interfaces
    Signed-off-by: Sylvia Lei <lixlei@microsoft.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/oras-project/oras-credentials-go
 
 go 1.19
+
+require oras.land/oras-go/v2 v2.0.2
+
+require golang.org/x/sync v0.1.0 // indirect
---
+++ store.go
@@ -0,0 +1,32 @@
+/*
+Copyright The ORAS Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package credentials
+
+import (
+	"context"
+
+	"oras.land/oras-go/v2/registry/remote/auth"
+)
+
+// Store is the interface that any credentials store must implement.
+type Store interface {
+	// Put saves credentials into the store for the given server address.
+	Put(ctx context.Context, serverAddress string, cred auth.Credential) error
+	// Delete removes credentials from the store for the given server address.
+	Delete(ctx context.Context, serverAddress string) error
+	// Get retrieves credentials from the store for the given server address.
+	Get(ctx context.Context, serverAddress string) (auth.Credential, error)
+}
