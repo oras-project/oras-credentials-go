e02b9c8ce4e295bcda23f367b8e5e38c91cdf8ef
Sylvia Lei <lixlei@microsoft.com>
2023-03-30 19:25:00 +0800

    draft get
    Signed-off-by: Sylvia Lei <lixlei@microsoft.com>

--- file_store.go
+++ file_store.go
@@ -2,6 +2,12 @@ package credentials
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+	"strings"
 
 	"oras.land/oras-go/v2/registry/remote/auth"
 )
@@ -10,14 +16,48 @@ import (
 // to keep the credentials in plain-text.
 type FileStore struct {
 	configPath  string
+	data        map[string]interface{}
 	DisableSave bool
 }
 
+const (
+	ConfigFieldAuths = "auths"
+)
+
+var (
+	ErrCredentialNotFound = errors.New("credential not found")
+)
+
+// AuthConfig contains authorization information for connecting to a Registry
+type AuthConfig struct {
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+	Auth     string `json:"auth,omitempty"`
+	// IdentityToken is used to authenticate the user and get
+	// an access token for the registry.
+	IdentityToken string `json:"identitytoken,omitempty"`
+	// RegistryToken is a bearer token to be sent to a registry
+	RegistryToken string `json:"registrytoken,omitempty"`
+}
+
 // NewFileStore creates a new file credentials store.
-func NewFileStore(configPath string) Store {
-	return &FileStore{
-		configPath: configPath,
+func NewFileStore(configPath string) (Store, error) {
+	configFile, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
 	}
+	defer configFile.Close()
+
+	fs := &FileStore{configPath: configPath}
+	jsonObj, err := ioutil.ReadAll(configFile)
+	if err != nil {
+		return nil, err
+	}
+	if err = json.Unmarshal(jsonObj, &fs.data); err != nil {
+		return nil, err
+	}
+
+	return fs, nil
 }
 
 // Put saves credentials into the store
@@ -31,6 +71,66 @@ func (fs *FileStore) Delete(ctx context.Context, serverAddress string) error {
 }
 
 // Get retrieves credentials from the store for the given server
-func (fs *FileStore) Get(ctx context.Context, serverAddress string) (auth.Credential, error) {
-	panic("not implemented") // TODO: Implement
+func (fs *FileStore) Get(_ context.Context, serverAddress string) (auth.Credential, error) {
+	cred := auth.Credential{}
+	authConfig, err := fs.getAuthConfig(serverAddress)
+	if err != nil {
+		return auth.EmptyCredential, err
+	}
+	cred.Username, cred.Password, err = decodeAuth(authConfig.Auth)
+	if err != nil {
+		return auth.EmptyCredential, err
+	}
+	cred.RefreshToken = authConfig.IdentityToken
+	cred.AccessToken = authConfig.RegistryToken
+	return cred, nil
+}
+
+func (fs *FileStore) getAuthConfig(serverAddress string) (AuthConfig, error) {
+	authsMap, ok := fs.data[ConfigFieldAuths].(map[string]interface{})
+	if !ok {
+		return AuthConfig{}, ErrCredentialNotFound
+	}
+	authConfigMap, ok := authsMap[serverAddress].(map[string]interface{})
+	if !ok {
+		return AuthConfig{}, ErrCredentialNotFound
+	}
+
+	var authConfig AuthConfig
+	for k, v := range authConfigMap {
+		switch k {
+		case "auth":
+			authConfig.Auth = v.(string)
+		case "identitytoken":
+			authConfig.IdentityToken = v.(string)
+		case "registrytoken":
+			authConfig.RegistryToken = v.(string)
+		}
+	}
+
+	return authConfig, nil
+}
+
+// decodeAuth decodes a base64 encoded string and returns username and password
+func decodeAuth(authStr string) (string, string, error) {
+	if authStr == "" {
+		return "", "", nil
+	}
+
+	decLen := base64.StdEncoding.DecodedLen(len(authStr))
+	decoded := make([]byte, decLen)
+	authByte := []byte(authStr)
+	n, err := base64.StdEncoding.Decode(decoded, authByte)
+	if err != nil {
+		return "", "", err
+	}
+	if n > decLen {
+		return "", "", errors.New("something went wrong decoding auth config")
+	}
+	arr := strings.SplitN(string(decoded), ":", 2)
+	if len(arr) != 2 {
+		return "", "", errors.New("invalid auth configuration file")
+	}
+	password := strings.Trim(arr[1], "\x00")
+	return arr[0], password, nil
 }
---
+++ file_store_test.go
@@ -0,0 +1,130 @@
+package credentials
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+
+	"oras.land/oras-go/v2/registry/remote/auth"
+)
+
+type config struct {
+	path string
+	data map[string]interface{}
+}
+
+func (c *config) Get(hostName string) auth.Credential {
+	f, err := os.Open(c.path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	jsonObj, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = json.Unmarshal(jsonObj, &c.data)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	authItem := c.data["auths"].(map[string]interface{})
+	regItem := authItem[hostName].(map[string]interface{})
+	fmt.Println("registry:", hostName, " item:", regItem)
+	fmt.Println("registry:", hostName, "auth:", regItem["auth"])
+
+	cred := auth.Credential{}
+	if token, ok := regItem["identitytoken"]; ok {
+		tokenStr := token.(string)
+		cred.RefreshToken = tokenStr
+
+	}
+	return cred
+}
+
+func (c *config) Save(hostName string, cred auth.Credential) {
+	authItem := c.data["auths"].(map[string]interface{})
+	regItem := authItem[hostName].(map[string]interface{})
+	regItem["auth"] = cred.Username + ":" + cred.Password
+
+	authItem[hostName] = regItem
+	c.data["auths"] = authItem
+
+	updatedData, err := json.MarshalIndent(c.data, "", "    ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = ioutil.WriteFile(c.path, updatedData, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func TestGet(t *testing.T) {
+	config := &config{path: "config.test.json"}
+	config.Get("myacr.azurecr.io")
+	config.Get("myregistry.example.com")
+
+	config.Save("myregistry.example.com", auth.Credential{Username: "abc", Password: "123"})
+}
+
+func TestFileStore_Get(t *testing.T) {
+	ctx := context.Background()
+	fs, err := NewFileStore("config.test.json")
+	if err != nil {
+		t.Fatal("NewFileStore() error =", err)
+	}
+
+	tests := []struct {
+		name          string
+		serverAddress string
+		want          auth.Credential
+		wantErr       bool
+	}{
+		{
+			name:          "username + password",
+			serverAddress: "registry1.example.com",
+			want: auth.Credential{
+				Username: "username",
+				Password: "password",
+			},
+		},
+		{
+			name:          "identity token",
+			serverAddress: "registry2.example.com",
+			want: auth.Credential{
+				RefreshToken: "identity_token",
+			},
+		},
+		{
+			name:          "registry token",
+			serverAddress: "registry3.example.com",
+			want: auth.Credential{
+				RefreshToken: "registry_token",
+			},
+		},
+		{
+			name:          "empty",
+			serverAddress: "registry4.example.com",
+			want:          auth.EmptyCredential,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := fs.Get(ctx, tt.serverAddress)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FileStore.Get() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FileStore.Get() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
