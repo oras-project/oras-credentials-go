73423c1efb5ccc286ac4011bc25c4645962bdd55
wangxiaoxuan273 <wangxiaoxuan119@gmail.com>
2023-04-10 03:39:09 +0000

    refined testStore
    Signed-off-by: wangxiaoxuan273 <wangxiaoxuan119@gmail.com>

--- registry_test.go
+++ registry_test.go
@@ -20,6 +20,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"reflect"
 	"testing"
 
 	"oras.land/oras-go/v2/registry/remote"
@@ -27,13 +28,19 @@ import (
 )
 
 // testStore implements the Store interface, used for testing purpose.
-type testStore struct{}
+type testStore struct {
+	storage map[string]auth.Credential
+}
 
 func (t *testStore) Get(ctx context.Context, serverAddress string) (auth.Credential, error) {
-	return auth.Credential{}, nil
+	return t.storage[serverAddress], nil
 }
 
 func (t *testStore) Put(ctx context.Context, serverAddress string, cred auth.Credential) error {
+	if len(t.storage) == 0 {
+		t.storage = make(map[string]auth.Credential)
+	}
+	t.storage[serverAddress] = cred
 	return nil
 }
 
@@ -66,7 +73,6 @@ func TestLogin(t *testing.T) {
 		{
 			name:     "login succeeds",
 			ctx:      context.Background(),
-			store:    ns,
 			registry: *reg,
 			cred:     auth.Credential{Username: testUsername, Password: testPassword},
 			wantErr:  false,
@@ -74,7 +80,6 @@ func TestLogin(t *testing.T) {
 		{
 			name:     "login fails (nil context makes remote.Ping fails)",
 			ctx:      nil,
-			store:    ns,
 			registry: *reg,
 			cred:     auth.Credential{Username: testUsername, Password: testPassword},
 			wantErr:  true,
@@ -82,8 +87,11 @@ func TestLogin(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := Login(tt.ctx, tt.store, tt.registry, tt.cred); (err != nil) != tt.wantErr {
-				t.Errorf("Login() error = %v, wantErr %v", err, tt.wantErr)
+			if err := Login(tt.ctx, ns, tt.registry, tt.cred); (err != nil) != tt.wantErr {
+				t.Fatalf("Login() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(ns.storage[tt.registry.Reference.Registry], tt.cred) {
+				t.Fatalf("Incorrect credentials stored")
 			}
 		})
 	}
