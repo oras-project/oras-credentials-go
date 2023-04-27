ddb2f19ef460fed00273f965a6b1d50a4f9a6ee2
Xiaoxuan Wang <wangxiaoxuan119@gmail.com>
2023-04-27 05:48:35 +0000

    docs: add examples for login and logout
    Signed-off-by: Xiaoxuan Wang <wangxiaoxuan119@gmail.com>

--- example_test.go
+++ example_test.go
@@ -15,72 +15,38 @@ package credentials_test
 
 import (
 	"context"
-	"encoding/base64"
 	"fmt"
-	"net/http"
-	"net/http/httptest"
-	"net/url"
-	"os"
-	"testing"
 
 	credentials "github.com/oras-project/oras-credentials-go"
 	"oras.land/oras-go/v2/registry/remote"
 	"oras.land/oras-go/v2/registry/remote/auth"
 )
 
-// testStore implements the Store interface, used for testing purpose.
-type testStore struct {
-	storage map[string]auth.Credential
-}
-
-func (t *testStore) Get(ctx context.Context, serverAddress string) (auth.Credential, error) {
-	return t.storage[serverAddress], nil
-}
-
-func (t *testStore) Put(ctx context.Context, serverAddress string, cred auth.Credential) error {
-	if len(t.storage) == 0 {
-		t.storage = make(map[string]auth.Credential)
-	}
-	t.storage[serverAddress] = cred
-	return nil
-}
-
-func (t *testStore) Delete(ctx context.Context, serverAddress string) error {
-	delete(t.storage, serverAddress)
-	return nil
-}
-
-var exampleRegistry *remote.Registry
-
-func TestMain(m *testing.M) {
-	// create a test registry
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		wantedAuthHeader := "Basic " + base64.StdEncoding.EncodeToString([]byte("test_username"+":"+"test_password"))
-		authHeader := r.Header.Get("Authorization")
-		if authHeader != wantedAuthHeader {
-			w.Header().Set("Www-Authenticate", `Basic realm="Test Server"`)
-			w.WriteHeader(http.StatusUnauthorized)
-		}
-	}))
-	defer ts.Close()
-	uri, _ := url.Parse(ts.URL)
-	exampleRegistry, err := remote.NewRegistry(uri.Host)
+func ExampleLogin() {
+	store, err := credentials.NewStore("example/path/config.json", credentials.StoreOptions{AllowPlaintextPut: true})
 	if err != nil {
 		panic(err)
 	}
-	exampleRegistry.PlainHTTP = true
-	os.Exit(m.Run())
-}
-
-func ExampleLogin_withBasicAuth() {
-	// create a test store
-	s := &testStore{}
-	basicAuthCredentials := auth.Credential{Username: "test_username", Password: "test_password"}
-	err := credentials.Login(context.Background(), s, exampleRegistry, basicAuthCredentials)
+	registry, err := remote.NewRegistry("example.registry.com")
 	if err != nil {
-		panic(err) // Handle error
+		panic(err)
+	}
+	cred := auth.Credential{Username: "username-example", Password: "password-example"}
+	err = credentials.Login(context.Background(), store, registry, cred)
+	if err != nil {
+		panic(err)
 	}
 	fmt.Println("Login succeeded")
-	// Output:
-	// Login succeeded
+}
+
+func ExampleLogout() {
+	store, err := credentials.NewStore("example/path/config.json", credentials.StoreOptions{AllowPlaintextPut: true})
+	if err != nil {
+		panic(err)
+	}
+	err = credentials.Logout(context.Background(), store, "example.registry.com")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Logout succeeded")
 }
